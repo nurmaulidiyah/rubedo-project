8d5dec318ebee446513c2eef1bae327a6cda609f
Surya <surya@example.com>
2016-08-19 15:28:51 +0700

    first commit

---
+++ module/Rubedo/src/Rubedo/User/Authentication/Adapter/AbstractAdapter.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Rubedo\User\Authentication\Adapter;
+
+use Rubedo\Interfaces\User\Authentication\IAdapter;
+
+abstract class AbstractAdapter extends \Zend\Authentication\Adapter\AbstractAdapter implements IAdapter
+{
+
+
+}
